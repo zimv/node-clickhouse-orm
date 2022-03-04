724b3a2f5ee6ceab98445d859b41744740175981
Rainpia <577130347@qq.com>
2022-03-04 16:38:16 +0800

    feat: 新增 orm 入口验证

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ import { ClickHouse } from 'clickhouse';
 import Orm, { DbParams } from './orm';
 export { setLogService } from './log';// Singleton Pattern
 export { DATA_TYPE } from './constants';
-
+import { ErrorLog } from "./log";
 
 export interface InitParams {
   client: Object;// TimonKK/clickhouse config
@@ -15,9 +15,15 @@ export const ClickhouseOrm = ({
   db,
   debug = false
 }: InitParams) => {
+  // 2.0版本，db 数据结构由 string 改成 object
+  // 升级版本有可能创建错误的 undefined 库，以此提示用户注意更新使用方式
+  if (!db.name) {
+    ErrorLog("Database name is undefined. It should be string");
+
+    return;
+  }
   /**
    * new ClickHouse
    */
-  const conn = new Orm({ client: new ClickHouse(client), db, debug });
-  return conn;
+  return new Orm({ client: new ClickHouse(client), db, debug });
 }
